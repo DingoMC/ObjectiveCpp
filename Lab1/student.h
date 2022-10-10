9f60ffd34535a42642a06f363b970d6120b6acd7
DingoMC <martin701-2001@wp.pl>
2022-10-10 11:07:27 +0200

    GIT GIT

---
+++ Lab1/student.h
@@ -0,0 +1,20 @@
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+#include "person.h"
+class Student : public Person {
+    private:
+        string index;
+    public:
+        Student (string index1, string name1, string surname1, int age1) : Person(name1, surname1, age1) {
+            index1 = index;
+        }
+        void setIndex(string newIndex) {
+            index = newIndex;
+        }
+        string getIndex() {return index;}
+        void showInfoStudent () {
+
+        }
+};
+
+#endif // STUDENT_H_INCLUDED
---
+++ Lab2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main () {
+    cin.ignore();
+    cin.get();
+    return 0;
+}
